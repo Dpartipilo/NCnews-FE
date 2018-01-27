fcb2c710d8eed3bc334d2fccb0e739400f3542bf
Dpartipilo <Dpartipilo@hotmail.com>
2018-01-27 17:39:41 +0000

    Improved article design

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,26 +1,28 @@
 import React from "react";
 
-const Article = props => (
-  <div key={props.id} className="box">
+const Article = ({ created_by, from_topic, title, votes, id, body }) => (
+  <div key={id} className="box">
     {/* top leftside of the article */}
     <div className="level">
       <div className="level-left">
-        <p className="level-item">
-          Created by: <strong> {props.created_by}</strong>
-        </p>
+        <div className="level-item">
+          <p>
+            Created by: <strong> {created_by}</strong>
+          </p>
+        </div>
       </div>
 
       {/* top right side */}
       <div className="level-right">
-        <p className="level-item">{props.date}</p>
+        <p className="level-item">Topic: {from_topic.toUpperCase()}</p>
       </div>
     </div>
 
     {/* title and body of the article */}
     <div className="content">
-      <p className="title is-4">{props.title}</p>
-      <p>{props.body}</p>
-      <p className="subtitle">votes: {props.votes}</p>
+      <p className="title is-4">{title}</p>
+      <p>{body}</p>
+      <p className="subtitle">votes: {votes}</p>
     </div>
   </div>
 );
