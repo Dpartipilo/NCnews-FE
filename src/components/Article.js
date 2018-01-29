3b700887f5411002f01a654a15dcafcec0f7542f
Dpartipilo <Dpartipilo@hotmail.com>
2018-01-29 12:29:21 +0000

    Fixed id parameter on articles

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Article = ({ created_by, from_topic, title, votes, id, body }) => (
-  <div key={id} className="box">
+const Article = ({ created_by, from_topic, title, votes, _id, body }) => (
+  <div key={_id} className="box">
     {/* top leftside of the article */}
     <div className="level">
       <div className="level-left">
@@ -18,10 +19,12 @@ const Article = ({ created_by, from_topic, title, votes, id, body }) => (
       </div>
     </div>
 
-    {/* title and body of the article */}
+    {/* title of the article */}
     <div className="content">
-      <p className="title is-4">{title}</p>
-      <p>{body}</p>
+      <Link className="title is-4" to={`/articles/${_id}`}>
+        {title}
+      </Link>
+      <br />
       <p className="subtitle">votes: {votes}</p>
     </div>
   </div>
