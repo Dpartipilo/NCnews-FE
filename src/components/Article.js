4cc68029a8108215cf78feed0aae94d818be34e8
Dpartipilo <Dpartipilo@hotmail.com>
2018-01-29 15:24:59 +0000

    Refactored Article Component

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,28 +3,29 @@ import { Link } from "react-router-dom";
 
 const Article = ({ created_by, from_topic, title, votes, _id, body }) => (
   <div key={_id} className="box">
-    {/* top leftside of the article */}
+    {/* Article Head Left side*/}
     <div className="level">
       <div className="level-left">
-        <div className="level-item">
-          <p>
-            Created by: <strong> {created_by}</strong>
-          </p>
-        </div>
+        <p className="level-item">
+          Created by: <strong> {created_by}</strong>
+        </p>
       </div>
 
-      {/* top right side */}
+      {/* Head Right side */}
       <div className="level-right">
-        <p className="level-item">Topic: {from_topic.toUpperCase()}</p>
+        <p className="level-item">Topic: {from_topic}</p>
       </div>
     </div>
 
-    {/* title of the article */}
+    {/* Head Body */}
     <div className="content">
       <Link className="title is-4" to={`/articles/${_id}`}>
         {title}
       </Link>
-      <br />
+    </div>
+
+    {/* Article Footer */}
+    <div>
       <p className="subtitle">votes: {votes}</p>
     </div>
   </div>
