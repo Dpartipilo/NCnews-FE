8212eb30dcfebc63759ec89e05d36ec5d51b0e83
Dpartipilo <Dpartipilo@hotmail.com>
2018-01-30 12:43:06 +0000

    Added articleVoteUp, articleVoteDown functions to make a put request to increase and decrease the votes on articles

--- src/api/index.js
+++ src/api/index.js
@@ -20,3 +20,11 @@ export const getArticleById = article_id => {
 export const getAllCommentsByArticle = article_id => {
   return axios.get(`${REACT_APP_API_URL}/articles/${article_id}/comments`);
 };
+
+export const articleVoteUp = article_id => {
+  return axios.put(`${REACT_APP_API_URL}/articles/${article_id}?vote=up`);
+};
+
+export const articleVoteDown = article_id => {
+  return axios.put(`${REACT_APP_API_URL}/articles/${article_id}?vote=down`);
+};
